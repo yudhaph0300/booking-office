677c3dbd72671719af47a7c759c709265bfb3df9
Mohammad Yudha Pamungkas <93898512+yudhaph0300@users.noreply.github.com>
2022-06-01 18:03:59 +0700

    display improvements

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,48 @@
 <template>
     <v-row>
         <v-col class="pr-0 pb-0">
-            <v-img width="100%" src="../assets/img/background.png"/>
+            <v-img height="100%" src="../assets/img/background.png"/>
         </v-col>
         <v-col class="pl-0">
-            <v-card class="ma-16" flat>
-                <v-card-text class="text-center">
+            <v-card class="mx-12 my-16" flat>
+                <v-card-text class="text-center heading-1">
                     Masuk
                 </v-card-text>
 
-                <v-card-text class="pa-0">
+                <v-card-text class="pa-0 body-regular-2">
                     Email
                 </v-card-text>
                 <v-text-field
                  outlined
                  placeholder="Masukan email anda"
+                 class="body-regular-3"
                 />
 
-                <v-card-text class="pa-0">
+                <v-card-text class="pa-0 body-regular-2">
                     Kata Sandi
                 </v-card-text>
                 <v-text-field
                  outlined
                  placeholder="Masukan kata sandi anda"
+                 class="body-regular-3"
+                 type="password"
                 />
 
                 <v-row>
                     <v-col class="text-left py-0">
                         <v-checkbox
                         class="my-0 py-0"
-                        color="green"
+                        color="#121950"
                         >
                             <template v-slot:label>
-                                <div>
+                                <div class="body-regular-3">
                                     Simpan Kata Sandi
                                 </div>
                             </template>
                         </v-checkbox>
                     </v-col>
                     <v-col class="text-right py-0">
-                        <a href="#">Lupa kata sandi ?</a>
+                        <a class="body-regular-3 link-forger-pass text-decoration-none" href="#">Lupa kata sandi ?</a>
                     </v-col>
                 </v-row>
 
@@ -55,12 +58,12 @@
                     </v-col>
                 </v-row>
 
-                <v-card-text class="text-center">
+                <v-card-text class="text-center body-regular-2">
                     atau
                 </v-card-text>
 
-                <v-card-text class="text-center">
-                    Belum punya akun ? <a href="/register">Daftar</a>
+                <v-card-text class="text-center body-regular-2">
+                    Belum punya akun ? <a class="link-forger-pass text-decoration-none" href="/register">Daftar</a>
                 </v-card-text>
                 
                 
@@ -75,6 +78,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+ .heading-1 {
+     font-family: 'Nunito', sans-serif;
+     font-weight: 700 !important;
+     font-size: 32px !important;
+     color: #606FAA !important;
+ }
+ .body-regular-2 {
+     font-family: 'Work Sans', sans-serif;
+     font-weight: 400 !important;
+     font-size: 14px !important;
+     color: #121950 !important;
+ }
+ .body-regular-3 {
+     font-family: 'Work Sans', sans-serif;
+     font-weight: 400 !important;
+     font-size: 12px !important;
+     color: #121950 !important;
+ }
+ .link-forger-pass {
+     color: #455392 !important;
+ }
 </style>
